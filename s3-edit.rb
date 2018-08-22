dce9b8a550e947fb7a6d9bf8f59e31e335b28928
Tsubasa Takayama <tsubasatakayama511@gmail.com>
2018-08-22 21:17:11 +0900

    Bump version to v0.0.7

--- s3-edit.rb
+++ s3-edit.rb
@@ -2,10 +2,10 @@ require 'formula'
 
 class S3Edit < Formula
   homepage 'https://github.com/tsub/s3-edit'
-  version 'v0.0.6'
+  version 'v0.0.7'
 
-  url 'https://github.com/tsub/s3-edit/releases/download/v0.0.6/s3-edit_v0.0.6_darwin_amd64.tar.gz'
-  sha256 '207a67db96a5c14c3f9cb54644b093b3cf5aec2f2b5f15255f730e3564ff254c'
+  url 'https://github.com/tsub/s3-edit/releases/download/v0.0.7/s3-edit_v0.0.7_darwin_amd64.tar.gz'
+  sha256 '588c1c1fa11bf5692f75089e3d907b3a1d01db7a8898591932ace2c56cd632cb'
 
   def install
     bin.install 's3-edit'
