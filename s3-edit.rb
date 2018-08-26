806cf13d5cdf1105a0aa61e743cfbeb746c27439
goreleaserbot <goreleaser@carlosbecker.com>
2018-08-26 21:35:42 +0900

    Brew formula update for s3-edit version v0.0.11

--- s3-edit.rb
+++ s3-edit.rb
@@ -1,9 +1,9 @@
 class S3Edit < Formula
   desc "Edit directly a file on Amazon S3 in CLI"
   homepage "https://github.com/tsub/s3-edit"
-  url "https://github.com/tsub/s3-edit/releases/download/v0.0.10/s3-edit_0.0.10_darwin_amd64.tar.gz"
-  version "0.0.10"
-  sha256 "62c02f28e53fef0926507631446245b2b2b8f7d55e42d16bdc8ccd7605730d4f"
+  url "https://github.com/tsub/s3-edit/releases/download/v0.0.11/s3-edit_0.0.11_darwin_amd64.tar.gz"
+  version "0.0.11"
+  sha256 "4b87e8fecf54f690111f95d0e1c6e8efa6fc93a801eaefa2e0afa912bf6edf55"
 
   def install
     bin.install 's3-edit'
